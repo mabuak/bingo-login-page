8ee7ff39e439ecbbcc0cfbb8de89dd8ac95f5dc7
Fachruzi Ramadhan <mabuak@live.com>
2018-08-26 10:22:29 +0700

    Initial Commit

---
+++ Bingo Login Page/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bingo Login Page
+//
+//  Created by Fachruzi Ramadhan on 26/08/2018.
+//  Copyright © 2018 Fachruzi Ramadhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
